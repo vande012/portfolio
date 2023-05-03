bdfd9c310cd35df98a328cb9c364b589c4abca1a
Ryan <vandehey12@gmail.com>
2023-05-02 22:49:37 -0500

    first update

---
+++ schemas/index.ts
@@ -0,0 +1,7 @@
+import blockContent from './blockContent'
+import skills from './skills'
+import pageInfo from './pageInfo'
+import experience from './experience'
+import project from './project'
+
+export const schemaTypes = [ experience, skills, blockContent, pageInfo, project ]
